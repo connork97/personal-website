88fa35ad911693173b7e97ac700e9384cef0f1db
Connor Kormos <connorkormos97@gmail.com>
2023-02-09 19:49:48 -0800

    initial design/layout for CSS set

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const tween = KUTE.fromTo(
     '#wave-top-1a',
     {path: '#wave-top-1a'},
     {path: '#wave-top-2a'},
-    {repeat: 999, duration: 5000, yoyo: true}
+    {repeat: 999, duration: 6000, yoyo: true}
 )
 
 tween.start()
