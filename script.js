5a57b0d4d8f62afa25d589a4aa379f59e219e88e
Connor Kormos <connorkormos97@gmail.com>
2023-02-11 15:27:17 -0800

    added a decent bit of keyframes for a more interactive website design

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const tween = KUTE.fromTo(
     '#wave-top-1b',
     {path: '#wave-top-1b'},
     {path: '#wave-top-2b'},
-    {repeat: 999, duration: 6000, yoyo: true}
+    {repeat: 999, duration: 5000, yoyo: true}
 )
 
 tween.start()
