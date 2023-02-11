96956053c1cca62e17db987609e729566af26db2
Connor Kormos <connorkormos97@gmail.com>
2023-02-10 19:26:37 -0800

    most of first draft complete.  links to external sites causing problems to be dealt with later.

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const tween2 = KUTE.fromTo(
     '#wave-top-1b',
     {path: '#wave-top-1b'},
     {path: '#wave-top-2b'},
-    {repeat: 999, duration: 4000, yoyo: true}
+    {repeat: 999, duration: 6000, yoyo: true}
 )
 
 tween2.start()
