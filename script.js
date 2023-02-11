778c975f9a54bf47f8fb2ba3fc969bceb652e7a6
Connor Kormos <connorkormos97@gmail.com>
2023-02-10 16:58:16 -0800

    improved website layout, added several grids, images, etc. much more CSS added as well.  missed a couple commits prior to this one

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-const tween = KUTE.fromTo(
-    '#wave-top-1a',
-    {path: '#wave-top-1a'},
-    {path: '#wave-top-2a'},
-    {repeat: 999, duration: 6000, yoyo: true}
-)
-
-tween.start()
 
 
 const tween2 = KUTE.fromTo(
